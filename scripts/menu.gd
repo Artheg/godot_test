e4fe3b6a43dfa544d80863fef34b88068f108917
Artheg <artheg@gmail.com>
2021-02-28 16:31:27 +0300

    menu.gd, renamed settings_scene var

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,16 +1,16 @@
 extends Control
 
 export (PackedScene) var Settings_scene;
-var new_settings_scene;
+var settings_scene_instance;
 
 func _ready():
-	new_settings_scene = Settings_scene.instance();
+	settings_scene_instance = Settings_scene.instance();
 
 func _on_start_button_pressed():
 	get_tree().change_scene("res://scenes/game.tscn");
 
 func _on_settings_button_pressed():
-	add_child(new_settings_scene);
+	add_child(settings_scene_instance);
 
 func _on_exit_button_pressed():
 	get_tree().quit(0);
